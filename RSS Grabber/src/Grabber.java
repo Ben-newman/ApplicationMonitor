39e0bd0ca7acea833a50a74a018816bd855217dc
matthball <matthew.3.ball@bt.com>
2013-09-17 16:25:53 +0100

    Managed to find firefox match in title

--- RSS Grabber/src/Grabber.java
+++ RSS Grabber/src/Grabber.java
@@ -6,7 +6,17 @@ import java.util.ArrayList;
 import java.io.*;
 
 public class Grabber {
+	
+	static private ArrayList<Application> apps = new ArrayList<Application>();
+	
     public static void main(String[] args) throws Exception {
+    	
+    	apps.add(new Application("Chrome"));
+    	apps.add(new Application("Firefox"));
+    	apps.add(new Application("Safari"));
+    	apps.add(new Application("Skype"));
+    	apps.add(new Application("Opera"));
+    	
     	ArrayList<String> items = new ArrayList<String>();
         URL oracle = new URL("http://feeds.feedburner.com/filehippo?format=xml");
         BufferedReader in = new BufferedReader(
@@ -14,18 +24,26 @@ public class Grabber {
 
         String inputLine;
         while ((inputLine = in.readLine()) != null){
-            items.add(extractTagContent("item", inputLine));
+        	String input = extractTagContent("item", inputLine);
+        	
+        	if(input!=null)
+        		items.add(input);
         }
         
-        for(String s:items)
-        	System.out.println(s);
+        for(String item:items) {
+        	
+        	Application check = checkItem(item);
+        	if(check!=null)
+        		System.out.println("Match found: - "+check.getTitle());
+        }
         
         in.close();
     }
     
     public static String extractTagContent(String tagName, String extractFrom)
     {    	
-    	if(extractFrom.contains("<"+tagName+">")){
+    	if(extractFrom.contains("<"+tagName+">"))
+    	{
         	String[] temp = extractFrom.split("<"+tagName+">");
         	String[] temp2 = temp[1].split("</"+tagName+">");
         	return temp2[0];
@@ -34,11 +52,15 @@ public class Grabber {
     	return null;
     }
     
-    public static boolean checkItem(String item)
+    public static Application checkItem(String item)
     {
-    	//get title
     	String title = extractTagContent("title", item);
+    	for(Application app : apps) 
+    	{
+    		if(title.contains(app.getTitle()))
+    			return app;
+    	}
     	
-    	return false;    	
+    	return null;    	
     }
 }
